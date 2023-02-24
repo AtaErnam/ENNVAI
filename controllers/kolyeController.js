44ed995d8e66f8e3bd050fa593f69e71953d0fc8
AtaErnam <67603284+AtaErnam@users.noreply.github.com>
2023-02-24 16:36:02 +0300

    adding advanced api features

--- controllers/kolyeController.js
+++ controllers/kolyeController.js
@@ -1,37 +1,105 @@
 /* eslint-disable prettier/prettier */
 
-exports.getAllKolyes = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route is not yet defined!',
+const Kolye = require("./../models/kolyeModel");
+const APIFeatures = require("./../utils/apiFeatures");
+
+exports.getAllTours = async (req, res) => {
+  try {
+    // EXECUTE QUERY
+    const features = new APIFeatures(Kolye.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const kolyes = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: "success",
+      results: kolyes.length,
+      data: {
+        tours,
+      },
     });
-  };
-  
-  exports.getKolye = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route is not yet defined!',
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({
+      status: "fail",
+      message: err,
     });
-  };
-  
-  exports.createKolye = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route is not yet defined!',
+  }
+};
+
+exports.getTour = async (req, res) => {
+  try {
+    const kolye = await Kolye.findById(req.params.id);
+    // Tour.findOne({_id: req.params.id})
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        kolye,
+      },
     });
-  };
-  
-  exports.updateKolye = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route is not yet defined!',
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
     });
-  };
-  
-  exports.deleteKolye = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route is not yet defined!',
+  }
+};
+
+exports.createTour = async (req, res) => {
+  try {
+    const newKolye = await Kolye.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        kolye: newKolye,
+      },
     });
-  };
-  
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Invalid data sent!",
+    });
+  }
+};
+
+exports.updateTour = async (req, res) => {
+  try {
+    const kolye = await Kolye.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        kolye: kolye,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.deleteTour = async (req, res) => {
+  try {
+    await kolye.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
