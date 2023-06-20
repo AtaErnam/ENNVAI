2831c7a3885e9b428fea20fc9bb1b2756ef95e28
Ata Ernam <ataernam@sabanciuniv.edu>
2023-06-20 16:22:29 +0300

    added createOrder controller

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -43,3 +43,81 @@ exports.getOrder = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.createOrder = catchAsync(async (req, res, next) => {
+  let stock_error = false;
+
+  const orderItemsIds = Promise.all(
+    req.body.orderItems.map(async (orderItem) => {
+      let newOrderItem = new OrderItem({
+        quantity: orderItem.quantity,
+        product: orderItem.product,
+        user: req.user,
+      });
+
+      newOrderItem = await newOrderItem.save();
+      return newOrderItem._id;
+    })
+  );
+
+  console.log(typeof orderItemsIds);
+
+  const orderItemsIdsResolved = await orderItemsIds;
+
+  const totalPrices = await Promise.all(
+    orderItemsIdsResolved.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById({ _id: orderItemId }).populate(
+        "product",
+        "price"
+      );
+      console.log(orderItem.product);
+      const totalPrice = orderItem.product.price * orderItem.quantity;
+      return totalPrice;
+    })
+  );
+
+  const totalQuantities = await Promise.all(
+    orderItemsIdsResolved.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById(orderItemId).populate(
+        "product",
+        "quantity"
+      );
+
+      console.log(orderItem.product.id);
+      const currOrderProd = await Product.findById(orderItem.product.id);
+      console.log(currOrderProd.quantity_in_stocks);
+      if (currOrderProd.quantity_in_stocks < orderItem.quantity) {
+        stock_error = true;
+      }
+
+      const totalQuantity = orderItem.quantity;
+      return totalQuantity;
+    })
+  );
+
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+  const totalQuantity = totalQuantities.reduce((a, b) => a + b, 0);
+
+  if (stock_error) {
+    return next(new AppError("No enough stock", 403));
+  }
+
+  let order = new Order({
+    orderItems: orderItemsIdsResolved,
+    address: req.body.address,
+    status: req.body.status,
+    totalPrice: totalPrice,
+    totalQuantity: totalQuantity,
+    user: req.body.user,
+  });
+  order = await order.save();
+
+  if (!order) return res.status(400).send("the order cannot be created!");
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      product: order,
+    },
+  });
+});
